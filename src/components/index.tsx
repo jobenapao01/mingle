4f0a92058a953d5cb649aff0353b71e24bc6b866
joben <jobenapao10@gmail.com>
2024-07-24 22:05:26 +0800

    feat: default index

---
+++ src/components/index.tsx
@@ -0,0 +1,4 @@
+export { default as SignUpForm } from './auth/SignupForm';
+export { default as LoginForm } from './auth/LoginForm';
+export { default as LoadingButton } from './LoadingButton';
+export { default as PasswordInput } from './PasswordInput';
