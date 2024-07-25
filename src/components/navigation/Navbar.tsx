eb647f6cb54aa44c987a98792a1ae21f08567de0
joben <jobenapao10@gmail.com>
2024-07-25 15:56:30 +0800

    feat: Menu bar component

--- src/components/navigation/Navbar.tsx
+++ src/components/navigation/Navbar.tsx
@@ -3,7 +3,7 @@ import { SearchField, UserButton } from '@/components';
 
 const Navbar = () => {
 	return (
-		<header className='sticky top-0 z-10 bg-card shadow-md'>
+		<header className='sticky top-0 z-10 bg-primary-foreground shadow-md'>
 			<div className='max-w-7xl mx-auto flex items-center justify-center flex-wrap gap-5 px-5 py-3'>
 				<Link
 					href='/'
