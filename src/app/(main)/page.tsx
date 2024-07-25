51f009f7d7387bf8b7ad7565c457e109410b3e59
joben <jobenapao10@gmail.com>
2024-07-25 15:56:22 +0800

    feat: main page

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,5 +1,9 @@
 import Image from 'next/image';
 
 export default function Home() {
-	return <main>main</main>;
+	return (
+		<main className='w-full'>
+			<div className='w-full '>main</div>
+		</main>
+	);
 }
