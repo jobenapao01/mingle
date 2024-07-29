bf8e7f3822f6a7b0a4f6e4defed102cdadb5c1d1
joben <jobenapao10@gmail.com>
2024-07-29 17:46:06 +0800

    refactor: Home page

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,25 +1,12 @@
-import { PostEditor, Post } from '@/components';
-import prisma from '@/lib/prisma';
-import { postDataInclude } from '@/types';
+import { PostEditor, ForYouFeed } from '@/components';
 import TrendingSidebar from './_components/TrendingSidebar';
 
-export default async function Home() {
-	const posts = await prisma.post.findMany({
-		include: postDataInclude,
-		orderBy: {
-			createAt: 'desc',
-		},
-	});
+export default function Home() {
 	return (
 		<main className='w-full min-w-0 flex ga-5'>
 			<div className='w-full min-w-0 space-y-5'>
 				<PostEditor />
-				{posts.map((post) => (
-					<Post
-						post={post}
-						key={post.id}
-					/>
-				))}
+				<ForYouFeed />
 			</div>
 			<TrendingSidebar />
 		</main>
