5d375c6d51addf5cb5121888991941df852e0c46
joben <jobenapao10@gmail.com>
2024-07-30 20:52:00 +0800

    style: Menubar

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -15,7 +15,7 @@ const AuthLayout = async ({ children }: { children: React.ReactNode }) => {
 				<Navbar />
 
 				<main className='max-w-7xl mx-auto p-5 flex w-full grow gap-5'>
-					<MenuBar className='sticky top-[5.25rem] h-fit hidden sm:block flex-none space-y-3 rounded-2xl bg-primary-foreground px-3 py-5 lg:px-5 shadow-sm xl:w-80' />
+					<MenuBar className='sticky top-[5.25rem] h-fit hidden sm:block flex-none space-y-3 rounded-2xl bg-primary-foreground px-3 py-5 lg:px-5 shadow-md border dark:border-none xl:w-80' />
 					{children}
 				</main>
 				<MenuBar className='sticky bottom-0 flex w-full justify-center gap-5 border-t bg-primary-foreground p-3 sm:hidden' />
