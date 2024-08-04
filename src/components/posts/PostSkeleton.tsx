f198b80c181d9863eeff95cdbc94d967e8c41221
joben <jobenapao10@gmail.com>
2024-08-04 20:46:46 +0800

    style: Skeleton light mode

--- src/components/posts/PostSkeleton.tsx
+++ src/components/posts/PostSkeleton.tsx
@@ -2,7 +2,7 @@ import { Skeleton } from '../ui/skeleton';
 
 const PostSkeleton = () => {
 	return (
-		<div className='w-full animate-pulse space-y-3 rounded-2xl bg-primary-foreground p-5 shadow-sm'>
+		<div className='w-full animate-pulse space-y-3 rounded-2xl dark:bg-primary-foreground bg-slate-200 p-5 shadow-sm'>
 			<div className='flex flex-wrap gap-3'>
 				<Skeleton className='size-12 rounded-full ' />
 				<div className='space-y-1.5'>
