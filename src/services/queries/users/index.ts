8c493caff64f46f3ae8fee4e4714e94b3ee831ea
joben <jobenapao10@gmail.com>
2024-08-13 16:56:17 +0800

    feat: fetch user

--- src/services/queries/users/index.ts
+++ src/services/queries/users/index.ts
@@ -1,12 +1,27 @@
-import { getFollowerInfo } from '@/services/api/users';
+import { getFollowerInfo, getUser } from '@/services/api/users';
 import { FollowerInfo } from '@/types';
 import { useQuery } from '@tanstack/react-query';
+import { AxiosError } from 'axios';
 
 export const useFollowerInfoQuery = (userId: string, initialState: FollowerInfo) => {
 	return useQuery({
 		queryKey: ['follower-info', userId],
 		queryFn: () => getFollowerInfo(userId),
 		initialData: initialState,
+		staleTime: Infinity,
+	});
+};
+
+export const useGetUser = (username: string) => {
+	return useQuery({
+		queryKey: ['user-data', username],
+		queryFn: () => getUser(username),
+		retry(failureCount, error) {
+			if (error instanceof AxiosError && error?.response?.status === 404) {
+				return false;
+			}
+			return failureCount < 3;
+		},
 		staleTime: Infinity,
 	});
 };
